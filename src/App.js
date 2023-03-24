feda61de0c905b04d409d9da637a3732ba0a8648
Ashish Desai <desai.ashish76@gmail.com>
2023-03-24 17:02:34 -0400

    react router nested routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+  margin: '1rem', 
+  padding: '1rem', 
+  border: '2px solid black',
 }
 
-export default App;
+const navLinks = {
+  display: 'flex',
+  justifyContent: 'center',
+}
+
+function Home() {
+  return( 
+    <div style={{...style , borderColor: 'red'}}>
+      <h2>Home Screen</h2>
+    </div>
+)}
+
+function Bikes() {
+  return( 
+    <div style={{...style , borderColor: 'green'}}>
+      <h2>Bikes Screen</h2>
+      <nav style={navLinks}>
+        <Link to="moutainbike">Mountain Bike</Link>&nbsp;&nbsp;
+        <Link to="urbanbike">Urban Bike</Link>
+      </nav>
+      <Routes>
+        <Route path="moutainbike" element={<MountainBike />} /> 
+        <Route path="urbanbike" element={<UrbanBike />} /> 
+      </Routes>
+    </div>
+)}
+
+
+function MountainBike() {
+  return( 
+    <div style={{...style , borderColor: 'black'}}>
+      <h2>MountainBike Screen</h2>
+    </div>
+)}
+
+function UrbanBike() {
+  return( 
+    <div style={{...style , borderColor: 'blue'}}>
+      <h2>UrbanBike Screen</h2>
+    </div>
+)}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav style={navLinks}>
+        <Link to='/'>Home</Link>&nbsp;&nbsp;
+        <Link to='bikes'>Bikes</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/> 
+        <Route path="bikes/*" element={<Bikes />}/> 
+       </Routes>
+    </BrowserRouter>
+  );
+}
